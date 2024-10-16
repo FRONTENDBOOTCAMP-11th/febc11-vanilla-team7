dbd658112d06d8ecfae9a5ebeb8361abd64c7c2e
Saebom <spring180@naver.com>
2024-10-16 15:59:13 +0900

    Feat: 폴더 구조 & 환경 세팅 (#14)

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'), // 기본 index.html
+        home: resolve(__dirname, 'src/views/home.html'),
         // login: resolve(__dirname, 'src/pages/auth/login.html'), // 추가 HTML 파일
         // list: resolve(__dirname, 'src/pages/board/list.html'), // 추가 HTML 파일
         // 필요한 다른 HTML 파일을 여기에 추가
