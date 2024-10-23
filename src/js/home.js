1fe037ed1af26aa513a266c118483746c946032c
Saebom <spring180@naver.com>
2024-10-23 11:52:10 +0900

    Feat: 요즘 뜨는 브런치 연동 성공 (#47)

--- src/js/home.js
+++ src/js/home.js
@@ -16,4 +16,30 @@ async function brunchData(url) {
 
 brunchData(`${url}/posts`).then(data => {
   const brunches = data.item;
+  const HOT_BRUNCH = 5;
+  const container = document.getElementById('brunch-container');
+
+  brunches.forEach((brunch, idx) => {
+    if (idx >= HOT_BRUNCH) return;
+    console.log(brunch, idx);
+
+    let brunchNode = document.createElement('div');
+    brunchNode.innerHTML = `
+        <div class="pl-1 pt-6 c-border-b flex items-start gap-3.5">
+          <img src="/src/assets/numbers/${idx + 1}.svg" />
+          <div class="pb-5">
+            <h3 class="c-text-17 leading-6">${brunch.title}</h3>
+            <span class="c-text-writer text-sm font-light leading-5"
+              ><italic class="font-georgia c-text-by italic">by</italic>
+              ${brunch.user.name}</span
+            >
+            <p class="c-text-content text-sm font-light leading-5 pt-5">
+              ${brunch.content}
+            </p>
+          </div>
+          <img src="/src/assets/img/book.png" />
+        </div>
+    `;
+    container.appendChild(brunchNode);
+  });
 });
