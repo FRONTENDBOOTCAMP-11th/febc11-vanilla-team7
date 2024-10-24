6b3456133be614649e1f99e3e2ae306a8328f413
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-10-24 16:09:32 +0900

    fix: DOM 노드 오타로 오류난 것 수정

--- src/js/post.js
+++ src/js/post.js
@@ -4,7 +4,7 @@ let subscript = document.getElementById('subscriptionImg');
 let likeNo = document.getElementById('likeNo');
 let likeImg = document.getElementById('likeImg');
 let subBtn = document.getElementById('subBtn');
-let likeBtn = document.getElementById('liekBtn');
+let likeBtn = document.getElementById('likeBtn');
 
 export function subscription() {
   if (subscript.src.includes('subscription.svg')) {
