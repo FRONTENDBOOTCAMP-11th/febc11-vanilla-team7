9af92620654261da615c85faaaa3428b310a0d7b
chayoutae <cdx8722@gmail.com>
2024-10-24 15:28:49 +0900

    11

--- src/js/write.js
+++ src/js/write.js
@@ -1,5 +1,3 @@
-function initializePage() {
-<<<<<<< 55-to-do-글쓰기-페이지-수정-및-기능-구현
 "use strict";
 
 // DOM 노드 추가
@@ -18,52 +16,30 @@ const alignmentStates = {
         next: 'center',
         imgSrc: 'src/assets/icons/align-left.svg',
         style: 'text-left'
-=======
-  console.log('write');
-  ('use strict');
-
-  // DOM 노드 추가
-  const alignButton = document.getElementById('align');
-  const title = document.getElementById('title');
-  const subtitle = document.getElementById('subtitle');
-  const contents = document.getElementById('contents');
-  const keyboardButton = document.getElementById('keyboard');
-
-  // 정렬 상태를 추적하는 변수
-  let alignState = 'left'; // 초기 상태는 왼쪽 정렬
-
-  // 정렬 상태에 따른 이미지 경로와 정렬 스타일 일치시키기
-  const alignmentStates = {
-    left: {
-      next: 'center',
-      imgSrc: 'src/assets/icons/align-left.svg',
-      style: 'text-left',
->>>>>>> develop
     },
-    center: {
-      next: 'right',
-      imgSrc: 'src/assets/icons/align-center.svg',
-      style: 'text-center',
+    'center': {
+        next: 'right',
+        imgSrc: 'src/assets/icons/align-center.svg',
+        style: 'text-center'
     },
-    right: {
-      next: 'left',
-      imgSrc: 'src/assets/icons/align-right.svg',
-      style: 'text-right',
-    },
-  };
+    'right': {
+        next: 'left',
+        imgSrc: 'src/assets/icons/align-right.svg',
+        style: 'text-right'
+    }
+};
 
-  // 정렬 변경 함수
-  function changeAlignment() {
+// 정렬 변경 함수
+function changeAlignment() {
     // 다음 정렬 상태로 업데이트
     alignState = alignmentStates[alignState].next;
-
+    
     // 이미지 변경
     alignButton.querySelector('img').src = alignmentStates[alignState].imgSrc;
-
+    
     // 이전 정렬 클래스 모두 제거
     title.classList.remove('text-left', 'text-center', 'text-right');
     subtitle.classList.remove('text-left', 'text-center', 'text-right');
-<<<<<<< 55-to-do-글쓰기-페이지-수정-및-기능-구현
     content.classList.remove('text-left', 'text-center', 'text-right');
     
     // 새로운 정렬 클래스 추가
@@ -75,20 +51,7 @@ const alignmentStates = {
 // 정렬 버튼 클릭 이벤트 리스너
 alignButton.addEventListener('click', changeAlignment);
 
-=======
-    contents.classList.remove('text-left', 'text-center', 'text-right');
 
-    // 새로운 정렬 클래스 추가
-    title.classList.add(alignmentStates[alignState].style);
-    subtitle.classList.add(alignmentStates[alignState].style);
-    contents.classList.add(alignmentStates[alignState].style);
-  }
->>>>>>> develop
-
-  // 정렬 버튼 클릭 이벤트 리스너
-  alignButton.addEventListener('click', changeAlignment);
-
-<<<<<<< 55-to-do-글쓰기-페이지-수정-및-기능-구현
 // 모든 필드의 내용 확인 함수
 function checkAllFields() {
     const titleFilled = title.value.trim() !== '';
@@ -102,17 +65,9 @@ function checkAllFields() {
     const keyboardImg = keyboardButton.querySelector('img');
     if (allFieldsFilled) {
         keyboardImg.src = 'src/assets/icons/keyboard-on.svg';
-=======
-  // 키보드 아이콘 상태 변경 함수
-  function updateKeyboardIcon(isFocused) {
-    const keyboardImg = keyboardButton.querySelector('img');
-    if (isFocused) {
-      keyboardImg.src = 'src/assets/icons/keyboard-on.svg';
->>>>>>> develop
     } else {
-      keyboardImg.src = 'src/assets/icons/keyboard-hide.svg';
+        keyboardImg.src = 'src/assets/icons/keyboard-hide.svg';
     }
-<<<<<<< 55-to-do-글쓰기-페이지-수정-및-기능-구현
 }
 
 // input 이벤트 리스너 등록 (실시간으로 내용 변화 감지)
@@ -156,29 +111,9 @@ async function postBrunchData(url, data) {
     } catch (error) {
         console.error('데이터 전송 중 오류 발생:', error);
         throw error;
-=======
-  }
-
-  // 포커스 이벤트 리스너
-  function handleFocus() {
-    updateKeyboardIcon(true);
-  }
-
-  function handleBlur(event) {
-    // 다른 입력 필드로 포커스가 이동하는 경우를 체크
-    const focusedElement = event.relatedTarget;
-    const isStillFocused =
-      focusedElement === title ||
-      focusedElement === subtitle ||
-      focusedElement === contents;
-
-    if (!isStillFocused) {
-      updateKeyboardIcon(false);
->>>>>>> develop
     }
-  }
+}
 
-<<<<<<< 55-to-do-글쓰기-페이지-수정-및-기능-구현
 const submitButton = document.getElementById('done');
 
 submitButton.addEventListener('click', async () => {
@@ -212,7 +147,7 @@ submitButton.addEventListener('click', async () => {
 
 
 
-//관리자로 로그인
+//로그인
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjEsInR5cGUiOiJhZG1pbiIsIm5hbWUiOiLrrLTsp4AiLCJlbWFpbCI6ImFkbWluQGZlc3Auc2hvcCIsImltYWdlIjoiL2ZpbGVzL3ZhbmlsbGEwNy91c2VyLW11emkud2VicCIsImxvZ2luVHlwZSI6ImVtYWlsIiwiaWF0IjoxNzI5NjY2MzcyLCJleHAiOjE3Mjk3NTI3NzIsImlzcyI6IkZFU1AifQ.6aVH5caA1jBit60e7R0cmhlTEyBHJzfRITvOaeYtcy8';
 async function loginUser() {
     const postUrl = 'https://11.fesp.shop/posts';
@@ -251,53 +186,3 @@ async function loginUser() {
       // 에러 처리
       console.error('Error:', error);
     });
-}
-=======
-  // 이벤트 리스너 등록
-  alignButton.addEventListener('click', changeAlignment);
-
-  // 포커스/블러 이벤트 리스너 등록
-  [title, subtitle, contents].forEach(element => {
-    element.addEventListener('focus', handleFocus);
-    element.addEventListener('blur', handleBlur);
-  });
-
-  async function postBrunchData(url, data) {
-    const response = await fetch(`${url}/posts`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'client-id': 'vanilla07',
-      },
-      body: JSON.stringify({
-        type: 'brunch', // brunch 타입으로 저장
-        title: data.title,
-        subtitle: data.subtitle,
-        content: data.content,
-      }),
-    });
-    return response.json();
-  }
-
-  //관리자로 로그인
-  const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjEsInR5cGUiOiJhZG1pbiIsIm5hbWUiOiLrrLTsp4AiLCJlbWFpbCI6ImFkbWluQGZlc3Auc2hvcCIsImltYWdlIjoiL2ZpbGVzL3ZhbmlsbGEwNy91c2VyLW11emkud2VicCIsImxvZ2luVHlwZSI6ImVtYWlsIiwiaWF0IjoxNzI5NjY2MzcyLCJleHAiOjE3Mjk3NTI3NzIsImlzcyI6IkZFU1AifQ.6aVH5caA1jBit60e7R0cmhlTEyBHJzfRITvOaeYtcy8';
-  const postUrl = 'https://11.fesp.shop/post';
-
-  fetch(postUrl, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(res => {
-      if (!res.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return res.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-}
->>>>>>> develop
