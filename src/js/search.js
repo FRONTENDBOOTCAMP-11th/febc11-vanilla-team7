acd0d84ad35471ce76fb388b48a166a7d4272f7d
Saebom <spring180@naver.com>
2024-10-28 10:27:45 +0900

    Feat: 검색 페이지 데이터 연결 (#71)

--- src/js/search.js
+++ src/js/search.js
@@ -1,3 +1,38 @@
 function initializePage() {
   console.log('search');
+
+  const url = 'https://11.fesp.shop';
+
+  async function searchData(url) {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'client-id': 'vanilla07',
+      },
+    });
+
+    return response.json();
+  }
+
+  function renderSearch() {
+    const inputNode = document.getElementById('search');
+
+    inputNode.addEventListener('input', event => {
+      const keyword = event.target.value;
+      console.log(keyword);
+
+      searchData(`${url}/posts?type=brunch&keyword=${keyword}`).then(data => {
+        const brunches = data.item;
+
+        brunches.forEach(brunch => {
+          console.log(brunch.title);
+          console.log(brunch.content);
+          console.log(brunch.createdAt);
+          console.log(brunch.user.name);
+        });
+      });
+    });
+  }
+  renderSearch();
 }
