10811c4f47513b16fd476fe11dd1f698d3df7c8a
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-10-29 15:48:30 +0900

    feat: 좋아요 시 북마크 등록, 좋아요 취소 시 북마크 삭제

--- src/js/post.js
+++ src/js/post.js
@@ -1,11 +1,42 @@
 import { subscription } from './subscript';
+
+let idNo;
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjEsInR5cGUiOiJhZG1pbiIsIm5hbWUiOiLrrLTsp4AiLCJlbWFpbCI6ImFkbWluQGZlc3Auc2hvcCIsImltYWdlIjoiL2FwaS9kYmluaXQtc2FtcGxlL2JydW5jaC91cGxvYWRGaWxlcy91c2VyLW11emkud2VicFxuIiwibG9naW5UeXBlIjoiZW1haWwiLCJpYXQiOjE3MzAwOTkxMzAsImV4cCI6MTczMDE4NTUzMCwiaXNzIjoiRkVTUCJ9.yFsdgpFNx4oxQL3y4tRll6Cn9pi772WqaXKenUuJDl0';
+let url = 'https://11.fesp.shop';
+
+async function bookmark(url) {
+  const res = await fetch(`${url}/post`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'client-id': 'vanilla07',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return res.json();
+}
+
+async function getId() {
+  const data = await bookmark(`${url}/bookmarks`);
+  console.log(data);
+  const id = data.item[0]._id;
+  console.log(id);
+  return id;
+}
+
+getId().then(id => {
+  console.log(id);
+  idNo = id;
+});
+
 export function post() {
   'use strict';
 
   const postId = window.pageId;
 
   console.log('postID!!!' + postId);
-  let url = 'https://11.fesp.shop';
+  // let url = 'https://11.fesp.shop';
 
   async function detailData(url) {
     const response = await fetch(url, {
@@ -20,7 +51,7 @@ export function post() {
   }
 
   function renderDetail() {
-    detailData(`${url}/posts/${postId}`).then(data => {
+    detailData(`${url}posts/${postId}`).then(data => {
       console.log(data);
     });
   }
@@ -33,12 +64,50 @@ export function post() {
   function like() {
     let likeNo = document.getElementById('likeNo');
     let likeImg = document.getElementById('likeImg');
-    if (likeImg.src.includes('like.svg')) {
-      likeImg.src = 'src/assets/icons/like_off.svg';
-      likeNo.innerHTML--;
-    } else {
+    if (likeImg.src.includes('like_off.svg')) {
       likeImg.src = 'src/assets/icons/like.svg';
       likeNo.innerHTML++;
+
+      async function bookmarkUser(url) {
+        const res = await fetch(`${url}/post`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'client-id': 'vanilla07',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({
+            target_id: 3,
+            memo: '좋아요',
+            extra: {
+              type: 'post',
+              like: likeNo.innerHTML,
+            },
+          }),
+        });
+        return res.json();
+      }
+      bookmarkUser(`${url}/bookmarks`).then(data => {
+        console.log(data);
+      });
+    } else {
+      likeImg.src = 'src/assets/icons/like_off.svg';
+      likeNo.innerHTML--;
+
+      async function bookmarkDelete(url) {
+        const res = await fetch(`${url}/${idNo++}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            'client-id': 'vanilla07',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        return res.json();
+      }
+      bookmarkDelete(`${url}/bookmarks`).then(data => {
+        console.log(data);
+      });
     }
   }
 
