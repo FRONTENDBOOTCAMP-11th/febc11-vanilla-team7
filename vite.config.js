e475a5dfad2b385315b4595a0daed116f7528f5b
Saebom <spring180@naver.com>
2024-10-31 13:11:24 +0900

    fix(vite.config.js): 수정

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: 'index.html', // 기본 index.html
-        home: 'src/views/home.html',
+        // home: 'src/views/home.html',
         // login: resolve(__dirname, 'src/pages/auth/login.html'), // 추가 HTML 파일
         // list: resolve(__dirname, 'src/pages/board/list.html'), // 추가 HTML 파일
         // 필요한 다른 HTML 파일을 여기에 추가
