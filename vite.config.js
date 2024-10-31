e0dd62fdc68480bf1d2089c30b9379fd3142ee2b
Saebom <spring180@naver.com>
2024-10-31 13:17:04 +0900

    fix(vite.config.js): html 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,18 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: 'index.html', // 기본 index.html
-        // home: 'src/views/home.html',
+        home: 'src/views/home.html',
+        header: 'src/components/header.html',
+        footer: 'src/components/footer.html',
+        login: 'src/views/login.html',
+        mybox: 'src/views/myBox.html',
+        post: 'src/views/post.html',
+        search: 'src/views/searc.html',
+        signup: 'src/views/signup.html',
+        upload: 'src/views/upload.html',
+        write: 'src/views/write.html',
+        writerHome: 'src/views/writerHome.html',
+
         // login: resolve(__dirname, 'src/pages/auth/login.html'), // 추가 HTML 파일
         // list: resolve(__dirname, 'src/pages/board/list.html'), // 추가 HTML 파일
         // 필요한 다른 HTML 파일을 여기에 추가
